73a47bf8db37a4901e1d237fd44c91d35ac8a819
Tobiah Zarlez <TobiahZ@users.noreply.github.com>
2016-09-13 11:05:02 -0700

    Updated WSA build script

--- jenkinswsa.bat
+++ jenkinswsa.bat
@@ -1,9 +1,10 @@
-cd Builds
+cd ../
 mkdir tmp
 cd tmp
-git clone tobiah:3gxtjtmsts2nusi75jbrpnzg5dlmpbhdhecs7ilv7d6nlcmgo5oa@https://usdxhackathon-devops.visualstudio.com/_git/JenkinsBuildPipeline 
-git rm -r *
-cp -R ../WSA/*
+git clone https://tobiah:3gxtjtmsts2nusi75jbrpnzg5dlmpbhdhecs7ilv7d6nlcmgo5oa@usdxhackathon-devops.visualstudio.com/_git/JenkinsBuildPipeline repo
+cd repo
+git rm -r .
+cp -R ../../BouncyBallGame/Builds/WSA/* ./
 git add .
 git commit -m "New version!"
 git push
